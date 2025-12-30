73b7c0d9f893cd21a283d1b00e231bc48fc0a120
Nethsarani   Sooriyaarachchi <nethsaranisooriyaarachchi89@gmail.com>
2025-12-30 13:50:41 +0530

    Add Laravel backend and connect frontend homepage

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nandana Tea - Experience the Essence of Ceylon</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
         footer { margin-top: 0; }
@@ -54,6 +54,6 @@
         </div>
     </footer>
 
-    <script src="main.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
 </html>
